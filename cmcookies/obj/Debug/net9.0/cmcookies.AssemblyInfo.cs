8b5a7edcf032a601a42454d28f6d4c6ab5fc1408
caml <eduardoquant07@gmail.com>
2025-11-27 21:53:07 -0600

    Sabra la Mona, es lit casi lo mismo que tenia pero con unos files randoms por ahi...

--- cmcookies/obj/Debug/net9.0/cmcookies.AssemblyInfo.cs
+++ cmcookies/obj/Debug/net9.0/cmcookies.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cmcookies")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f8b6796844bf9257411f96fb2fb002c06a16f41")]
 [assembly: System.Reflection.AssemblyProductAttribute("cmcookies")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cmcookies")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
