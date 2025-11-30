6ac494dce187692aa9e22118234598a31f47ade6
caml <eduardoquant07@gmail.com>
2025-11-30 11:48:00 -0600

    Debbug, se añadieron 4 tablas nuevas a la DB para arreglar problemas relacionados a Core.Identity a la hora de hacer loggin

--- cmcookies/obj/Debug/net9.0/cmcookies.AssemblyInfo.cs
+++ cmcookies/obj/Debug/net9.0/cmcookies.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cmcookies")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0152add74f0ae32b1f35c6d8b7c16ed96801d1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc0de3933735831952c1e5a736f82c6f37ca6694")]
 [assembly: System.Reflection.AssemblyProductAttribute("cmcookies")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cmcookies")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
